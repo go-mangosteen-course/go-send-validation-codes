f49c35e13c50c2806af639fd930cb51d8a2029f2
FrankFang <frankfang1990@gmail.com>
2023-04-26 22:11:15 +0800

    引入 Swagger 并生成 API 文档

--- internal/controller/ping.go
+++ internal/controller/ping.go
@@ -6,6 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Ping godoc
+// @Summary      用来测试 API 是否正常工作
+// @Description  用来测试 API 是否正常工作
+// @Accept       json
+// @Produce      json
+// @Success      200
+// @Failure      500
+// @Router       /ping [get]
 func Ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -3,13 +3,34 @@ package router
 import (
 	"mangosteen/internal/controller"
 
+	"mangosteen/docs"
+
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"     // swag
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
+
+// @title           山竹记账 API
+// @description     这是一个使用 Swagger 2.0 标准编写的 API 文档。
+
+// @contact.name   方应杭
+// @contact.url    https://fangyinghang.com
+// @contact.email  fangyinghang@foxmail.com
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth(JWT)
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 
 func New() *gin.Engine {
 	r := gin.Default()
+	docs.SwaggerInfo.Version = "1.0"
 
-	r.GET("/ping", controller.Ping)
+	r.GET("/api/v1/ping", controller.Ping)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return r
 }
