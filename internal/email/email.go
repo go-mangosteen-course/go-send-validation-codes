3f4334937811e67ffc2bb8f0ec39c8ede6a580a8
FrankFang <frankfang1990@gmail.com>
2023-04-26 23:32:20 +0800

    把 STMP 重命名为 SMTP

--- internal/email/email.go
+++ internal/email/email.go
@@ -9,8 +9,8 @@ import (
 )
 
 var (
-	EMAIL_STMP_HOST = os.Getenv("EMAIL_SMTP_HOST")
-	EMAIL_STMP_PORT = os.Getenv("EMAIL_SMTP_PORT")
+	EMAIL_SMTP_HOST = os.Getenv("EMAIL_SMTP_HOST")
+	EMAIL_SMTP_PORT = os.Getenv("EMAIL_SMTP_PORT")
 	EMAIL_USER      = os.Getenv("EMAIL_USER")
 	EMAIL_PWD       = os.Getenv("EMAIL_PWD")
 )
@@ -22,10 +22,10 @@ func Send() {
 	m.SetHeader("Subject", "Hello!")
 	m.SetBody("text/html", "Hello <b>方应杭</b>!")
 	var d *gomail.Dialer
-	if port, err := strconv.Atoi(EMAIL_STMP_PORT); err != nil {
+	if port, err := strconv.Atoi(EMAIL_SMTP_PORT); err != nil {
 		log.Fatalln("EMAIL_STMP_PORT is not a number")
 	} else {
-		d = gomail.NewDialer(EMAIL_STMP_HOST, port, EMAIL_USER, EMAIL_PWD)
+		d = gomail.NewDialer(EMAIL_SMTP_HOST, port, EMAIL_USER, EMAIL_PWD)
 	}
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
